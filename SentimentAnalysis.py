622ae91f7a4b9515e86f2b1e8bfa6bf273e0bc61
wxiea <43360560+wxiea@users.noreply.github.com>
2018-09-17 20:45:32 -0600

    Add files via upload

---
+++ SentimentAnalysis.py
@@ -0,0 +1,44 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import praw
+
+
+reddit = praw.Reddit(client_id= 'usC8J2OisZW27g',
+                     client_secret= 'YCuh26wmTLJcZsh62ZfXfj46Sh4',
+                     user_agent= 'wxiea' 
+                     )
+
+
+nltk.download('vader_lexicon')
+sid = SentimentIntensityAnalyzer()
+
+
+def get_text_negative_proba(text):
+   return sid.polarity_scores(text)['neg']
+
+
+def get_text_neutral_proba(text):
+   return sid.polarity_scores(text)['neu']
+
+
+def get_text_positive_proba(text):
+   return sid.polarity_scores(text)['pos']
+
+
+def get_submission_comments(url):
+    submission = reddit.submission(url=url)
+    submission.comments.replace_more()
+
+    return submission.comments
+
+
+def main():
+    comments = get_submission_comments('https://www.reddit.com/r/learnprogramming/comments/5w50g5/eli5_what_is_recursion/')
+    print(comments[0].body)
+    print(comments[0].replies[0].body)
+
+    neg = get_text_negative_proba(comments[0].replies[0].body)
+
+    print(neg)
+
+main()
